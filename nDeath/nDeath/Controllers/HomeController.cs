072de550a42108a658b7e2438e6a17b1053b5888
Maxim <sipakovmaksim@gmail.com>
2019-02-25 15:45:48 +0300

    Add folder nDeath with solution

---
+++ nDeath/nDeath/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using nDeath.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nDeath.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Parabola parabola)
+        {
+            if (parabola.X1 > parabola.X2)
+            {
+                ModelState.AddModelError("X1", "X1 must be less than X2");
+            }
+            if (ModelState.IsValid)
+            {
+                List<int> points = new List<int>();
+
+                int y;
+                
+                for (int x = parabola.X1; x <= parabola.X2; x += parabola.Step)
+                {
+                    y = parabola.A * x * x + parabola.B * x + parabola.C;
+                    points.Add(x);
+                    points.Add(y);
+                }
+
+                string jsonFile = "points.json";
+                System.IO.File.WriteAllText(Server.MapPath(jsonFile), JsonConvert.SerializeObject(points));
+
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ nDeath/nDeath/Models/Parabola.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nDeath.Models
+{
+    public class Parabola
+    {
+        [Required(ErrorMessage = "Enter the coefficient A")]
+        public int A{ get; set; }
+
+        [Required (ErrorMessage = "Enter the coefficient B")]
+        public int B { get; set; }
+
+        [Required(ErrorMessage = "Enter the coefficient C")]
+        public int C { get; set; }
+
+        [Required(ErrorMessage = "Enter step")]
+        public int Step { get; set; }
+        
+        [Required(ErrorMessage = "Enter X1")]
+        public int X1 { get; set; }
+
+        [Required(ErrorMessage = "Enter X2")]
+        public int X2 { get; set; }
+    }
+}
